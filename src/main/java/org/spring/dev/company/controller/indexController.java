d63794333ae7f84ea055b590ecab4d8028b09b6b
yejin <rladpwls1843@gmail.com>
2023-10-16 12:29:51 +0900

    javascript

--- src/main/java/org/spring/dev/company/controller/indexController.java
+++ src/main/java/org/spring/dev/company/controller/indexController.java
@@ -1,10 +1,12 @@
 package org.spring.dev.company.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.spring.dev.company.dto.workTime.WorkTimeDto;
 import org.spring.dev.company.entity.board.BoardEntity;
 import org.spring.dev.company.entity.util.BaseEntity;
 import org.spring.dev.company.entity.util.BoardType;
 import org.spring.dev.company.repository.board.BoardRepository;
+import org.spring.dev.company.service.workTime.WorkTimeService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -13,7 +15,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("")
@@ -22,6 +26,7 @@ import java.util.List;
 public class indexController {
 
     private final BoardRepository boardRepository;
+    private final WorkTimeService workTimeService;
 
 //    main 페이지
 
@@ -33,7 +38,9 @@ public class indexController {
                         @RequestParam(name = "boardType", required = false, defaultValue = "GENERAL") BoardType boardType,
                         @PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
                         @RequestParam(value = "search", required = false) String search,
-                        @ModelAttribute("searchHistory") List<String> searchHistory){
+                        @ModelAttribute("searchHistory") List<String> searchHistory,
+//                        @PathVariable("memberId") Long memberId,
+                        @RequestParam(value = "workType", required = false) String workType){
 //boardlist가져오기
         Page<BoardEntity> boardList;
         if (search != null && !search.isEmpty()) {
@@ -56,6 +63,12 @@ public class indexController {
         model.addAttribute("boardList", boardList);
         model.addAttribute("boardType", boardType);
         model.addAttribute("key", "boardList");
+
+//        오늘의 근무 가져오기
+//        Map<String, Object> map = new HashMap<>();
+
+        // 근무기록 list로 가져오기(반환)
+//        List<WorkTimeDto> workTimeList = workTimeService.getWorkTimeWorkList(memberId,workType);
 
 
         return "index";
--- src/main/resources/static/js/workTime/workTime.js
+++ src/main/resources/static/js/workTime/workTime.js
@@ -50,6 +50,37 @@
                   setCalendar(work_type,start_date,end_date,work_id);
                   getCalendar(calendar.getDate() ,memberId)
                   calendar.render();
+                  location.reload(location.href);
+
+                }
+              
+              })
+
+              $('.btn-close').click(function(){
+                $('.modal').fadeOut();
+              })
+            }
+          },
+          workAddButton:{
+            text : '근무 추가',
+            click:function(){
+              $('.modal').fadeIn();
+
+              $('.btn-add').click(function(){
+
+                let work_type = $("#calendar_work_type").val();
+                let start_date = $("#calendar_start_date").val();
+                let end_date = $("#calendar_end_date").val();
+                let work_id = 1
+
+                if(start_date == "" || end_date == ""){
+                  alert("날짜를 선택해주세요!");
+                } else if(end_date - start_date < 0 ){
+                  alert("종료 시간이 시작 날짜보다 먼저입니다.");
+                } else{
+                  setCalendar(work_type,start_date,end_date,work_id);
+                  getCalendar(calendar.getDate() ,memberId)
+                  calendar.render();
 
                 }
               
@@ -61,10 +92,11 @@
             }
           }
         },
+        
         headerToolbar: {
           left: 'prev,next today',
           center: 'title',
-          right: 'workUpdateButton'
+          right: 'workUpdateButton workAddButton'
         }
 
       });
