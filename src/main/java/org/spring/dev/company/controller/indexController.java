57af416c682203d1afa775152c45b0fa3c06c6e5
yejin <rladpwls1843@gmail.com>
2023-10-16 11:14:03 +0900

    add board img

--- src/main/java/org/spring/dev/company/controller/indexController.java
+++ src/main/java/org/spring/dev/company/controller/indexController.java
@@ -1,18 +1,63 @@
 package org.spring.dev.company.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.spring.dev.company.entity.board.BoardEntity;
+import org.spring.dev.company.entity.util.BaseEntity;
+import org.spring.dev.company.entity.util.BoardType;
+import org.spring.dev.company.repository.board.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("")
+@RequiredArgsConstructor
+@SessionAttributes("searchHistory")
 public class indexController {
+
+    private final BoardRepository boardRepository;
+
+//    main 페이지
 
     /*
     추후 멤버정보 가져오는거 수정필요
      */
     @GetMapping({"/","/index"})
-    public String index(){
+    public String index(Model model,
+                        @RequestParam(name = "boardType", required = false, defaultValue = "GENERAL") BoardType boardType,
+                        @PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                        @RequestParam(value = "search", required = false) String search,
+                        @ModelAttribute("searchHistory") List<String> searchHistory){
+//boardlist가져오기
+        Page<BoardEntity> boardList;
+        if (search != null && !search.isEmpty()) {
+            searchHistory.add(search);
+        }
+        if(search != null && !search.isEmpty()){
+            boardList = boardRepository.findByBoardTypeAndTitleContaining(boardType, search, pageable);
+        }else{
+            boardList = boardRepository.findByBoardType(boardType, pageable);
+        }
+        Long totalCount = boardList.getTotalElements();
+        int pageSize = boardList.getSize();
+        int nowPage = boardList.getNumber();
+        int totalPage = boardList.getTotalPages();
+        int blockNum = 3;
+        int startPage = (int)((Math.floor(nowPage/blockNum)) + 1 < totalPage ? (Math.floor(nowPage/blockNum) * blockNum) + 1 : totalPage);
+        int endPage = (startPage + blockNum - 1 < totalPage ? startPage + blockNum - 1 : totalPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("boardList", boardList);
+        model.addAttribute("boardType", boardType);
+        model.addAttribute("key", "boardList");
+
+
         return "index";
     }
 }
