309e74345989cfa6ea5465c38d63f9b7ab899844
yejin <rladpwls1843@gmail.com>
2023-10-16 21:12:54 +0900

    pay controller

--- src/main/java/org/spring/dev/company/controller/indexController.java
+++ src/main/java/org/spring/dev/company/controller/indexController.java
@@ -1,6 +1,7 @@
 package org.spring.dev.company.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.spring.dev.company.config.MyUserDetails;
 import org.spring.dev.company.dto.workTime.WorkTimeDto;
 import org.spring.dev.company.entity.board.BoardEntity;
 import org.spring.dev.company.entity.util.BaseEntity;
@@ -11,10 +12,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,31 +29,34 @@ import java.util.Map;
 public class indexController {
 
     private final BoardRepository boardRepository;
-    private final WorkTimeService workTimeService;
 
 //    main 페이지
 
     /*
     추후 멤버정보 가져오는거 수정필요
      */
+    @ModelAttribute("searchHistory") // 검색 이력을 세션에 저장하기 위한 모델 어트리뷰트
+    public List<String> initializeSearchHistory() {
+        return new ArrayList<>();
+    }
+
     @GetMapping({"/","/index"})
     public String index(Model model,
+                        @AuthenticationPrincipal MyUserDetails myUserDetails,
                         @RequestParam(name = "boardType", required = false, defaultValue = "GENERAL") BoardType boardType,
                         @PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
                         @RequestParam(value = "search", required = false) String search,
-                        @ModelAttribute("searchHistory") List<String> searchHistory,
-//                        @PathVariable("memberId") Long memberId,
-                        @RequestParam(value = "workType", required = false) String workType){
+                        @ModelAttribute("searchHistory") List<String> searchHistory){
 //boardlist가져오기
         Page<BoardEntity> boardList;
-        if (search != null && !search.isEmpty()) {
-            searchHistory.add(search);
-        }
+
         if(search != null && !search.isEmpty()){
+            searchHistory.add(search);
             boardList = boardRepository.findByBoardTypeAndTitleContaining(boardType, search, pageable);
         }else{
             boardList = boardRepository.findByBoardType(boardType, pageable);
         }
+
         Long totalCount = boardList.getTotalElements();
         int pageSize = boardList.getSize();
         int nowPage = boardList.getNumber();
@@ -58,17 +64,17 @@ public class indexController {
         int blockNum = 3;
         int startPage = (int)((Math.floor(nowPage/blockNum)) + 1 < totalPage ? (Math.floor(nowPage/blockNum) * blockNum) + 1 : totalPage);
         int endPage = (startPage + blockNum - 1 < totalPage ? startPage + blockNum - 1 : totalPage);
+
+
+//        MemberDto member = memberService.detailMember(myUserDetails.getMemberEntity().getMemberId());
+//        model.addAttribute("member", member);
+
+
         model.addAttribute("startPage", startPage);
         model.addAttribute("endPage", endPage);
         model.addAttribute("boardList", boardList);
         model.addAttribute("boardType", boardType);
         model.addAttribute("key", "boardList");
-
-//        오늘의 근무 가져오기
-//        Map<String, Object> map = new HashMap<>();
-
-        // 근무기록 list로 가져오기(반환)
-//        List<WorkTimeDto> workTimeList = workTimeService.getWorkTimeWorkList(memberId,workType);
 
 
         return "index";
--- src/main/java/org/spring/dev/company/controller/pay/PayController.java
+++ src/main/java/org/spring/dev/company/controller/pay/PayController.java
@@ -2,6 +2,7 @@ package org.spring.dev.company.controller.pay;
 
 import lombok.RequiredArgsConstructor;
 import org.spring.dev.company.dto.pay.PayDto;
+import org.spring.dev.company.dto.workTime.WorkTimeDto;
 import org.spring.dev.company.service.pay.PayService;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,8 +18,9 @@ public class PayController {
     private final PayService payService;
 
     // 정산하기 버튼 눌렀을 떄 들어가기
+//    월급 달마다
     @PostMapping("/{memberId}")
-    public Map<String,Object> getMemberPayPrice(
+    public Map<String,Object> getMemberPayMontly(
             @PathVariable("memberId") Long memberId,
             @RequestParam(value = "workMonth", required = false) String workMonth){
 
@@ -29,12 +31,20 @@ public class PayController {
         map.put("result", result);
         return map;
     }
+    @GetMapping("/{memberId}/yearList")
+    public List<PayDto> getMemberPayYearly(
+            @PathVariable("memberId") Long memberId,
+            @RequestParam(value = "workYear", required = false) String workYear){
 
-//    월급 달마다 보여주기
-//    total만 보여주는지 다 보여주는지 -> 다 보여주는 거로 결정!
-//    정산하기 버튼 누르면 나오는거??
+        // 년에 해당하는 근무기록 가져오기
+        List<PayDto> result = payService.getPayYearList(memberId, workYear);
+
+        return result;
+    }
+
+// 해당 멤버에 모든 월급내역 보여주기
     @GetMapping("/{memberId}/list")
-    public Map<String, Object> getMemberPayMontly(
+    public Map<String, Object> getMemberPayPrice(
             @PathVariable("memberId") Long memberId){
 
         // 월급 list로 가져오기
@@ -44,4 +54,5 @@ public class PayController {
         map.put("payList", payDtoList);
         return map;
     }
+
 }
