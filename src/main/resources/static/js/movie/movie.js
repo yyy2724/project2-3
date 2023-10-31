a5888c041a26585663b11208aafdf35b7f1b5ca7
catfather <catfather49@gmail.com>
2023-10-31 17:45:38 +0900

    ajax 처리

---
+++ src/main/resources/static/js/movie/movie.js
@@ -0,0 +1,31 @@
+$.ajax({
+
+    type: "GET", // HTTP method type(GET, POST) 형식이다.
+    url: "/movie/detail/" + "20234664",
+    success: function (result) {
+
+        $('#').val('원하는 값');
+        result.audiAcc
+        result.audiChange
+        result.audiCnt
+        result.audiInten
+        result.movieCd
+        result.movieNm
+        result.openDt
+        result.rank
+        result.rankInten
+        result.rankOldAndNew
+        result.rnum
+        result.salesAcc
+        result.salesAmt
+        result.salesChange
+        result.salesInten
+        result.salesShare
+        result.scrnCnt
+        result.showCnt
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+        alert("통신 실패.")
+    }
+
+})
