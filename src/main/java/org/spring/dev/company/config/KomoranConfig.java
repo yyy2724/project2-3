0e16cdd958249c57d03a5ac42de9e56935ca488f
yejin <rladpwls1843@gmail.com>
2023-11-02 12:29:12 +0900

    movie design

--- src/main/java/org/spring/dev/company/config/KomoranConfig.java
+++ src/main/java/org/spring/dev/company/config/KomoranConfig.java
@@ -64,11 +64,11 @@ public class KomoranConfig {
         });
 
 
-        keys.add("¼­¿ï");
-        keys.add("ºÎ»ê");
-        keys.add("±¤ÁÖ");
-        keys.add("ÃáÃµ");
-        keys.add("³¯¾¾");
+        keys.add("ì„œìš¸");
+        keys.add("ë¶€ì‚°");
+        keys.add("ê´€ì£¼");
+        keys.add("ì¶˜ì²œ");
+        keys.add("ë‚ ì”¨");
 
 
         try {
